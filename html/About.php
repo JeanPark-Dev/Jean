e688f2936b41b78916a045f2a5e8ec2b3188d627
qkrwls5998 <qkrwls5998@gmail.com>
2017-08-11 01:55:32 +1200

    s
    s

---
+++ html/About.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+
+<title>Jinyoung Park | Software Developer | Christchurch</title>
+
+<body>
+    <div id="mySidenav" class="sidenav">
+        <?php
+            echo file_get_contents("http://qkrwls5998.github.io/data/list.txt")
+         ?>
+    </div>
+
+    <header>
+        <h1>About Me</h1>
+    </header>
+
+
+</body>
+</html>
